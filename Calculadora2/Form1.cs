ea88dbd3e4c5b725f922623a8a1013c02b9e8713
Williamgabrielsilv <willgabsil@gmail.com>
2025-09-14 22:07:58 -0300

    Update Form1.cs

--- Calculadora2/Form1.cs
+++ Calculadora2/Form1.cs
@@ -170,15 +170,37 @@ namespace Calculadora2
         }
         private bool ContemDivisaoPorZero(string expressao)
         {
-            // Remove espaços para facilitar a análise
             expressao = expressao.Replace(" ", "");
 
-            // Verifica se há "/0" ou "/0." (ex: 1/0, 1/0.0)
-            return expressao.Contains("/0");
+            // Procura por divisões por zero explícitas
+            for (int i = 0; i < expressao.Length - 1; i++)
+            {
+                if (expressao[i] == '/')
+                {
+                    string numero = "";
+                    int j = i + 1;
 
+                    while (j < expressao.Length && (char.IsDigit(expressao[j]) || expressao[j] == '.'))
+                    {
+                        numero += expressao[j];
+                        j++;
+                    }
 
+                    if (double.TryParse(numero, out double valor))
+                    {
+                        if (valor == 0.0)
+                            return true;
+                    }
+                }
+            }
 
+            return false;
         }
+
+
+
+
+
     }
 }
 
