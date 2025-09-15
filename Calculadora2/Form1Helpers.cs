983a055e7d00d26a4fe3dbff50bcbfbb787a6967
Williamgabrielsilv <willgabsil@gmail.com>
2025-09-14 22:52:25 -0300

    Create Form1Helpers.cs

---
+++ Calculadora2/Form1Helpers.cs
@@ -0,0 +1,36 @@
+﻿namespace Calculadora2
+{
+    internal static class Form1Helpers
+    {
+        private static bool ContemDivisaoPorZero(string expressao)
+        {
+            expressao = expressao.Replace(" ", "");
+            if (expressao.Contains("/0") || expressao.Contains("/0.") || expressao.Contains("/.0"))
+                return true;
+
+            /*// Procura por divisões por zero explícitas
+            for (int i = 0; i < expressao.Length - 1; i++)
+            {
+                if (expressao[i] == '/')
+                {
+                    string numero = "";
+                    int j = i + 1;
+
+                    while (j < expressao.Length && (char.IsDigit(expressao[j]) || expressao[j] == '.'))
+                    {
+                        numero += expressao[j];
+                        j++;
+                    }
+
+                    if (double.TryParse(numero, out double valor))
+                    {
+                        if (valor == 0.0)
+                            return true;
+                    }
+                }
+            }
+
+            return false;*/
+        }
+    }
+}
